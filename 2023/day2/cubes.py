296318ccdedc38a02c9fe31c3ff16f3db202385d
Gary Parsons <39571634+garypwn@users.noreply.github.com>
2024-01-12 21:07:25 -0800

    Solve day 2 part 1

---
+++ 2023/day2/cubes.py
@@ -0,0 +1,42 @@
+from enum import Enum
+import re
+from typing import Iterable
+
+colors = {"red", "green", "blue"}
+_pattern = re.compile(r"\s*(\d+)\s*(\w+)")
+
+
+class CubeGame:
+    sets: list[dict]
+
+    def __init__(self, line: str):
+        self.sets = []
+        for substr in line.split(';'):
+            s = {color: 0 for color in colors}
+            for set_str in substr.split(','):
+                n, color = _pattern.match(set_str).groups()
+                color = color.lower()
+                if color not in colors:
+                    raise ValueError()
+                s[color] = n
+
+            self.sets.append(s)
+
+    def max(self):
+        return {color: max([int(s[color]) for s in self.sets]) for color in colors}
+
+    def check_limit(self, limits):
+        maxes = self.max()
+        for color in colors:
+            if maxes[color] > limits[color]:
+                return False
+        return True
+
+
+def create_games(lines: Iterable[str]):
+    games = []
+    for line in lines:
+        seqs = line.split(':')
+        games.append(CubeGame(seqs[1]))
+
+    return games
---
+++ 2023/day2/part1.py
@@ -0,0 +1,13 @@
+from cubes import create_games, colors
+
+games = create_games(open('input.txt'))
+
+limits = {'red': 12, 'green': 13, 'blue': 14}
+
+total = 0
+for i, game in enumerate(games):
+    i += 1  # Games are 1-indexed
+    if game.check_limit(limits):
+        total += i
+
+print(f"The sum of game ids is {total}")
