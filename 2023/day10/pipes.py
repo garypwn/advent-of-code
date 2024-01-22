7e8c814abd748d001f66d9a3698bb91646eb6e0c
Gary Parsons <39571634+garypwn@users.noreply.github.com>
2024-01-22 13:03:22 -0800

    Solve day 10 part 2

--- 2023/day10/pipes.py
+++ 2023/day10/pipes.py
@@ -43,11 +43,30 @@ class Pipes:
                 return p
 
     def loop(self):
-        points = [self.start()]
+        start = self.start()
+        curr = start
+        last = None
         while True:
-            p = self.follow(points[-1], points[-2] if len(points) > 1 else None)
-            if p == points[0]:
-                return points
+            yield curr
+            p = self.follow(curr, last)
+            if p == start:
+                break
 
-            points.append(p)
+            last = curr
+            curr = p
 
+
+def area(loop):
+    # The area bounded by the inside edge of the pipes in the loop
+    a = 0
+    for prev, curr, ahead in zip(loop[-1:] + loop[:-1], loop, loop[1:] + loop[:1]):
+        match sub(curr, ahead):
+            case (0, 1):
+                value = 1
+            case (0, -1):
+                value = -1
+            case _:
+                value = 0
+
+        a += curr[0] * value
+    return abs(a) - len(loop) // 2 + 1
--- 2023/day10/solutions.py
+++ 2023/day10/solutions.py
@@ -1,6 +1,7 @@
-from pipes import Pipes
+from pipes import Pipes, area
 
 pipes = Pipes(open('input.txt'))
-loop = pipes.loop()
+loop = list(pipes.loop())
 print(f"Loop length: {len(loop)}")
 print(f"Farthest point: {len(loop) // 2}")
+print(f"Area: {area(loop)}")
