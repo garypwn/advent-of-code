5a68202ead1f8b540d9ff4e33c76c3e246babd11
Gary Parsons <39571634+garypwn@users.noreply.github.com>
2024-11-25 15:10:18 -0800

    Solve 2022/13 p1

---
+++ 2022/day_13/compare.py
@@ -0,0 +1,44 @@
+import json
+from functools import cmp_to_key
+
+from utils.aocd_solutions import Puzzle
+
+puzzle = Puzzle(2022, 13)
+
+def pairs(data):
+    # This can be done super easily with a json decoder
+    for pair in data.split('\n\n'):
+        yield [json.loads(p) for p in pair.split('\n')]
+
+def cmp(l: list | int, r: list | int):
+    # Here we implement the comparison logic set out by the puzzle
+    if isinstance(l, int) and isinstance(r, int):
+        return l - r
+    elif isinstance(l, int):
+        return cmp([l], r)
+    elif isinstance(r, int):
+        return cmp(l, [r])
+
+    # Empty lists
+    if len(l) == 0 and len(r) == 0:
+        return 0
+    elif len(l) == 0:
+        return -1
+    elif len(r) == 0:
+        return 1
+
+    # Lists with items
+    c = cmp(l[0], r[0])
+    return cmp(l[1:], r[1:]) if c == 0 else c
+
+@puzzle.solution_a
+def solve_p1(data):
+    correct_indices = []
+    for i, pair in enumerate(pairs(data)):
+        if cmp(*pair) <= 0:
+            correct_indices.append(i+1)
+
+    return sum(correct_indices)
+
+puzzle.check_examples()
+puzzle.check_solutions()
