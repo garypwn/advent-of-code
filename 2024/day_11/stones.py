92be1f80670a656cbc46da735f6531ab3e1276dc
Gary Parsons <39571634+garypwn@users.noreply.github.com>
2024-12-11 22:08:03 -0800

    Use some caching to make 2024/11 faster

--- 2024/day_11/stones.py
+++ 2024/day_11/stones.py
@@ -1,3 +1,5 @@
+from functools import cache
+
 from utils.aocd_solutions import Puzzle
 
 puzzle = Puzzle(2024, 11)
@@ -7,6 +9,13 @@ def parse(data):
     l = [int(s) for s in data.split(' ')]
     return {s: l.count(s) for s in l}
 
+@cache
+def digit_split(n):
+    s = str(n)
+    if len(s) % 2 == 0:
+        return int(s[:len(s) // 2]), int(s[len(s) // 2:])
+    else:
+        return (n * 2024,)
 
 def blink(stones):
     new = {1: 0}
@@ -15,16 +24,11 @@ def blink(stones):
             new[1] += ct
             continue
 
-        s = str(stone)
-        if len(s) % 2 == 0:
-            split = int(s[:len(s) // 2]), int(s[len(s) // 2:])
-            for p in split:
-                if p in new:
-                    new[p] += ct
-                else:
-                    new[p] = ct
-        else:
-            new[stone * 2024] = ct
+        for p in digit_split(stone):
+            if p in new:
+                new[p] += ct
+            else:
+                new[p] = ct
 
     return new
 
