edf9627cfe59b8e866f12e1e36e279c010f2d18b
Gary Parsons <39571634+garypwn@users.noreply.github.com>
2024-12-23 07:39:22 -0800

    2024/22: Solve part 2 with brute force

--- 2024/day_22/prng.py
+++ 2024/day_22/prng.py
@@ -1,3 +1,5 @@
+from itertools import pairwise
+
 import numpy as np
 
 from utils.aocd_solutions import Puzzle
@@ -26,5 +28,26 @@ def solve_p1(data):
         nums = prng(nums)
     return sum(int(n) for n in nums)
 
+@puzzle.solution_b
+def solve_p2(data):
+    nums = parse(data)
+    prices = []
+    for i in range(2000):
+        prices.append(nums % 10)
+        prng(nums)
+    changes = np.asarray([x - y for x, y in pairwise(prices)]).transpose()
+    sequences = dict()
+    for i in range(4, 2000):
+        seq = changes[:,i-4:i]
+        price = prices[i]
+        for j, (s, p) in enumerate(zip(seq, price)):
+            s = tuple(s)
+            if s in sequences:
+                if not j in sequences[s]:
+                    sequences[s][j] = p
+            else:
+                sequences[s] = {j: p}
+
+    return max(sum(v.values()) for v in sequences.values())
 
 puzzle.check_solutions()
