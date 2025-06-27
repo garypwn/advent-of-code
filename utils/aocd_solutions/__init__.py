f5a34f1ce3913a8c34c63fd5b5c8563155883df2
Gary Parsons <39571634+garypwn@users.noreply.github.com>
2025-06-27 11:09:50 -0700

    Let aocd queries use custom ca cert

--- utils/aocd_solutions/__init__.py
+++ utils/aocd_solutions/__init__.py
@@ -1,8 +1,27 @@
 import itertools
+import os
 from time import time
 from typing import Callable
 
 import aocd.models
+import certifi
+from urllib3 import PoolManager, ProxyManager
+
+# Dumb hack to inject CA certs into aocd if desired.
+# This is done by injecting a custom PoolManager into aocd's singleton http wrapper.
+# This will break if aocd ever changes that behavior.
+if ca_cert := os.environ.get('AOCD_CA_CERT', 0):
+    if ca_cert.lower() == 'certifi':
+        ca_cert = certifi.where()
+    http = aocd.utils.http
+    proxy = isinstance(aocd.utils.http.pool_manager, ProxyManager)
+    headers = http.pool_manager.headers
+    proxy_url = os.environ.get("http_proxy") or os.environ.get("https_proxy")
+
+    if proxy_url:
+        http.pool_manager = ProxyManager(proxy_url, headers=headers, ca_certs=ca_cert)
+    else:
+        http.pool_manager = PoolManager(headers=headers, ca_certs=ca_cert)
 
 
 class Puzzle(aocd.models.Puzzle):
@@ -29,7 +48,7 @@ class Puzzle(aocd.models.Puzzle):
                 if s != "Pass":
                     all_pass = False
                 if s:
-                    print(f"Example {i+1}A: {s}\t\t(t={time()-t}s)")
+                    print(f"Example {i + 1}A: {s}\t\t(t={time() - t}s)")
 
             if self._solution_funcs[1] and example.answer_b:
                 t = time()
@@ -37,7 +56,7 @@ class Puzzle(aocd.models.Puzzle):
                 if s != "Pass":
                     all_pass = False
                 if s:
-                    print(f"Example {i+1}B: {s}\t\t(t={time()-t}s)")
+                    print(f"Example {i + 1}B: {s}\t\t(t={time() - t}s)")
 
             if example.extra:
                 print(f"Note: {example.extra}\n")
@@ -49,20 +68,19 @@ class Puzzle(aocd.models.Puzzle):
         if self._solution_funcs[0]:
             t = time()
             result = self._solution_funcs[0](*itertools.chain((self.input_data,), args), **kwargs)
-            print(f"Part A result: {result}\t\t(t={time()-t}s)")
+            print(f"Part A result: {result}\t\t(t={time() - t}s)")
             if result is not None:
                 self.answer_a = result
 
         if self._solution_funcs[1]:
             t = time()
             result = self._solution_funcs[1](*itertools.chain((self.input_data,), args), **kwargs)
-            print(f"Part B result: {result}\t\t(t={time()-t}s)")
+            print(f"Part B result: {result}\t\t(t={time() - t}s)")
             if result is not None:
                 self.answer_b = result
 
 
 def _check_answer(f, data, answer, args=None, kwargs=None):
-
     if kwargs is None:
         kwargs = {}
     if args is None:
