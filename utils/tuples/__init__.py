56fb36fea81585fe32715832014be9f60183b146
Gary Parsons <39571634+garypwn@users.noreply.github.com>
2025-07-31 14:41:50 -0700

    Update Tuples utility for Python 3.12

--- utils/tuples/__init__.py
+++ utils/tuples/__init__.py
@@ -3,58 +3,59 @@ Tools for performing element-wise operations on tuples of hashable types
 """
 
 from functools import cache
-from typing import Any
+from typing import TypeVar, Tuple
 
+T = TypeVar('T', int, float)
 
 @cache
-def add[T](t1: tuple[T], t2: tuple[T]) -> tuple[Any, ...]:
+def add(t1: Tuple[T, ...], t2: Tuple[T, ...]) -> Tuple[T, ...]:
     """Element-wise add two tuples of the same length"""
     return tuple(a + b for a, b in zip(t1, t2))
 
 
 @cache
-def sub[T](t1: tuple[T], t2: tuple[T]) -> tuple[Any, ...]:
+def sub (t1: Tuple[T, ...], t2: Tuple[T, ...]) -> Tuple[T, ...]:
     """Element-wise subtract two tuples of the same length"""
     return tuple(a - b for a, b in zip(t1, t2))
 
 
 @cache
-def neg[T](t: tuple[T]) -> tuple[Any, ...]:
+def neg(t: Tuple[T, ...]) -> Tuple[T, ...]:
     """Negate all elements in a tuple"""
     return tuple(-a for a in t)
 
 
 @cache
-def dot[T](t1: tuple[T], t2: tuple[T]) -> T:
+def dot(t1: Tuple[T, ...], t2: Tuple[T, ...]) -> T:
     """Compute the dot product of two tuples of the same length"""
     return sum(a * b for a, b in zip(t1, t2))
 
 
 @cache
-def mod[T](t1: tuple[T], t2: tuple[T]) -> T:
+def mod(t1: Tuple[T, ...], t2: Tuple[T, ...]) -> Tuple[T, ...]:
     """Compute the modulo of two tuples of the same length"""
     return tuple(a % b for a, b in zip(t1, t2))
 
 
 @cache
-def mul[T](t1: tuple[T], t2: tuple[T]) -> T:
+def mul(t1: Tuple[T, ...], t2: Tuple[T, ...]) -> Tuple[T, ...]:
     """Compute the product of two tuples of the same length"""
     return tuple(a * b for a, b in zip(t1, t2))
 
 
 @cache
-def floordiv[T](t1: tuple[T], t2: tuple[T]) -> T:
+def floordiv(t1: Tuple[T, ...], t2: Tuple[T, ...]) -> Tuple[T, ...]:
     """Compute integer dividend of two tuples of the same length"""
     return tuple(a // b for a, b in zip(t1, t2))
 
 
 @cache
-def scale[T](t: tuple[T], s: T) -> T:
+def scale(t: Tuple[T, ...], s: T) -> Tuple[T, ...]:
     """Multiply a tuple t by a scalar s"""
     return tuple(a * s for a in t)
 
 
 @cache
-def sgn[T](t: tuple[T]) -> T:
+def sgn(t: Tuple[T, ...]) -> Tuple[T, ...]:
     """Applies the sign() function to each scalar"""
     return tuple((x > 0) - (x < 0) for x in t)
